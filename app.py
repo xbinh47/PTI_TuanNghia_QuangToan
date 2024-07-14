aa72dac643455dc7d155dac9febc2f0da27200e4
IHATETOVERIFYTHATIMNOTAROBOT <162005706+IHATETOVERIFYTHATIMNOTAROBOT@users.noreply.github.com>
2024-07-14 16:10:30 +0700

    abc

--- app.py
+++ app.py
@@ -1,40 +1,56 @@
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets 
 from PyQt6.QtWidgets import QMessageBox
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from PyQt6.QtCore import QUrl
 from PyQt6 import uic
 import sys
-
+import sqlite3
+import requests
+import datetime
+# http://openweathermap.org/img/w/04d.png
 class Register(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__ (self):
         super().__init__()
         uic.loadUi("ui/register.ui", self)
         self.name = ""
-        self.btnRegister.clicked.connect(self.Register)
-        self.btnLogin.clicked.connect(self.showLoginPage)
+        self.btnRegister.clicked.connect(self.register)
+        self.btnRegister_2.clicked.connect(self.showLoginPage)
 
-    def Register(self):
+    def register(self):
         self.name = self.txtName.text()
-        email = self.txtEmail.text
+        email = self.txtEmail.text()
         password = self.txtPassword.text()
-        comfirm_password = self.txtCPassword.text()
 
         if not self.name:
-            msg_box.setText("Vui lòng nhập tên!")
-            msg_box.exec
-            return
+            err_box.setText("Please enter your name!")
+            err_box.exec()
+            return 
+
         if not email:
-            msg_box.setText("Vui lòng nhập email!")
-            msg_box.exec
-            return
+            err_box.setText("Please enter your email!")
+            err_box.exec() 
+            return 
+
         if not password:
-            msg_box.setText("Vui lòng nhập mật khẩu!")
-            msg_box.exec()
-            return
-        if not self.agree.isChecked():
-            msg_box.setText("Vui lòng đọc và đồng ý với các điều khoản của AdoptMate!")
-            msg_box.exec
+            err_box.setText("Please enter your password!")
+            err_box.exec()
             return
         
-        success_box.setText("Bạn đã đăng kí tài khoản thành công!")
+        query = f"SELECT * FROM USER WHERE email = ('{email}')"
+        result = query_db(query)
+
+        if len(result) > 0:
+            err_box.setText("This email had been used for an another account!")
+            err_box.exec()
+            return
+        
+        query = f"INSERT INTO USER (name, password, email) VALUES ('{self.name}', '{password}', '{email}')"
+        print(query)
+        insert_db(query)
+
+        success_box.setText("Register Successfully!")
+        loginPage.show()
         self.close()
 
     def showLoginPage(self):
@@ -43,50 +59,71 @@ class Register(QtWidgets.QMainWindow):
 
 class Login(QtWidgets.QMainWindow):
     def __init__(self):
-        super().__init__()
-        uic.loadUi("ui/login.ui",self)
-        self.btnLogin.clicked.connect(self.check_login)
-        self.btnRegister.clicked.connect(self.show_register)
-
-    def check_login(self):
+        super().__init__() #call out the characters of ParentClass
+        uic.loadUi("ui/login.ui", self) #Create and load the file ui
+        self.name = ""
+        self.Login.clicked.connect(self.checkLogin)
+        self.btnRegister.clicked.connect(self.showRegisterPage)
+        
+    def checkLogin(self):
         email = self.txtEmail.text()
         password = self.txtPassword.text()
 
         if not email:
-            msg_box.setText("Vui lòng nhập lại email!")
-            msg_box.exec
+            err_box.setText("Please enter your email!")
+            err_box.exec()
             return
-    
+
         if not password:
-            msg_box.setText("Vui lòng nhập lại mật khẩu!")
-            msg_box.exec()
+            err_box.setText("Please enter your password!")
+            err_box.exec()
+            return
+
+        query = f"SELECT * FROM USER WHERE email ='{email}' and password='{password}'" #query select
+        result = query_db(query)
+
+        if len(result) == 0:
+            err_box.setText("Invalid Username or Password!")
+            err_box.exec()
             return
         
-        if email == "admin@gmail.com" and password == "123456":
-            success_box.setText("Đăng nhập thành công!")
-            success_box.exec()
-            return
-        else:
-            msg_box.setText("Email hoặc mật khẩu không đúng!")
-            msg_box.exec()
-            return
-        
-    def show_register(self):
-        registerPage
+        self.name = result[0][1]
+        success_box.setText("Succesfully Login!")
+        success_box.exec()
+        self.showMainPage()
+
+    def showRegisterPage(self):
+            registerPage.show()
+            self.close()
 
 if __name__ == '__main__':
+    sqliteConnection = sqlite3.connect('data/data.db')
+    def insert_db(query):
+        cusor = sqliteConnection.cursor()
+        cusor.execute(query)
+        sqliteConnection.commit()
+        cusor.close()
+
+    def query_db(query):
+        cursor = sqliteConnection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall() #check if the result was in db(in list)
+        cursor.close()
+        return result
     app = QtWidgets.QApplication(sys.argv)
 
     loginPage = Login()
     loginPage.show()
     registerPage = Register()
 
-    msg_box = QMessageBox()
-    msg_box.setWindowTitle("Error")
-    msg_box.setIcon(QMessageBox.Icon.Warning)
-
+    err_box = QMessageBox()
+    err_box.setWindowTitle("Error.")
+    err_box.setIcon(QMessageBox.Icon.Warning)
+    # msg_box.setStyleSheet()
+    
     success_box = QMessageBox()
-    success_box.setWindowTitle("Success")
+    success_box.setWindowTitle("Success!")
     success_box.setIcon(QMessageBox.Icon.Information)
-
+    # success_box.setStyleSheet
     app.exec()
+    
